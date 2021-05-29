c83e0a804843ede97016b704c085a41e0fd0ca3c
vijay <tech@hsad.co.in>
2021-05-29 15:13:22 +0530

    update
    vivek

--- src/app/pages/google-otp/google-otp.page.ts
+++ src/app/pages/google-otp/google-otp.page.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Events } from '../../services/events';
-import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { Events } from "../../services/events";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-google-otp',
-  templateUrl: './google-otp.page.html',
-  styleUrls: ['./google-otp.page.scss'],
+  selector: "app-google-otp",
+  templateUrl: "./google-otp.page.html",
+  styleUrls: ["./google-otp.page.scss"],
 })
 export class GoogleOtpPage implements OnInit {
-
   constructor(
     public events: Events,
     private authService: AuthService,
     public router: Router
-  ) { }
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   googleLogin() {
-		this.router.navigateByUrl('/ssid-login');
-	}
-
+    this.router.navigateByUrl("/ssid-login");
+  }
+  otpController(event, next, prev) {
+    if (event.target.value.length < 1 && prev) {
+      prev.setFocus();
+    } else if (next && event.target.value.length > 0) {
+      next.setFocus();
+    } else {
+      return 0;
+    }
+  }
 }
