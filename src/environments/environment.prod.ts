a39dde221d3f05e22736f4a6a87e117f297dffde
Abhishek Tiwari <abhishek.tiwari@satoopmedia.com>
2021-07-06 12:57:12 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyBBy8esHgx48ZUhqm8tbbOw_-ioua2pfwA",
+    authDomain: "arihant-75f6a.firebaseapp.com",
+    projectId: "arihant-75f6a",
+    storageBucket: "arihant-75f6a.appspot.com",
+    messagingSenderId: "1070455497289",
+    appId: "1:1070455497289:web:e150199dfc30d395f81598",
+    measurementId: "G-2F7SL8DT8G"
   }
 };
